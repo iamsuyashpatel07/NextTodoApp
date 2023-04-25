18b41e4b9f0d5c2b78d02a59fe8a7c72e3823e38
iamsuyashpatel07 <suyashpatelmrj@gmail.com>
2023-04-25 15:51:23 +0530

    New task Dynamic

--- pages/api/newtask.js
+++ pages/api/newtask.js
@@ -4,15 +4,24 @@ import { connectDB } from '../../utils/features';
 const handler = async (req, res) => {
   await connectDB();
 
+  if (req.method !== 'POST') {
+    return res.status(400).json({
+      success: false,
+      message: 'Only POST Method is allowed',
+    });
+  }
+  const { title, description } = req.body;
+
   await Task.create({
-    title: 'Sample Title 1',
-    description: 'Sample Description for title 1.',
+    title,
+    description,
     // isCompleted: false,
     user: 'SomeOneghjdsk',
   });
 
   res.json({
     success: true,
+    message: 'Task Created',
   });
 };
 
