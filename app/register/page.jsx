5a5b57fd1aa68ca8b84e8899e771cb26b69ca34d
iamsuyashpatel07 <suyashpatelmrj@gmail.com>
2023-04-25 06:53:54 +0530

    registr

---
+++ app/register/page.jsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import React from 'react';
+
+export const metadata = {
+  title: 'signup',
+  description: 'A app whwere you can store your Todo  List || signup Page',
+};
+
+const page = () => {
+  return (
+    <div className='login'>
+      <section>
+        <form>
+          <input type='text' placeholder='Enter Name' />
+          <input type='email' placeholder='Enter email' />
+          <input type='password' placeholder='Enter Password' />
+          <button type='submit'>Login</button>
+          <p>OR</p>
+          <Link href={'/login'}>Already Register</Link>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default page;
