9bf17a1362feb28a460b985aa7d6959e762a04a4
iamsuyashpatel07 <suyashpatelmrj@gmail.com>
2023-04-25 16:32:20 +0530

    Erro Handling Done

--- pages/api/newtask.js
+++ pages/api/newtask.js
@@ -1,14 +1,12 @@
+import { asyncError } from '../../middlewares/error';
 import { Task } from '../../models/task';
 import { connectDB } from '../../utils/features';
 
-const handler = async (req, res) => {
+const handler = asyncError(async (req, res) => {
   await connectDB();
 
   if (req.method !== 'POST') {
-    return res.status(400).json({
-      success: false,
-      message: 'Only POST Method is allowed',
-    });
+    return errorHandler(res, 400, 'Only POST Method is allowed');
   }
   const { title, description } = req.body;
 
@@ -23,6 +21,6 @@ const handler = async (req, res) => {
     success: true,
     message: 'Task Created',
   });
-};
+});
 
 export default handler;
