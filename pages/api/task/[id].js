535c237bf2f238a3982b8c4daeaec5035bd483d5
iamsuyashpatel07 <suyashpatelmrj@gmail.com>
2023-05-05 20:25:09 +0530

    Bakend Done

---
+++ pages/api/task/[id].js
@@ -0,0 +1,33 @@
+import { checkAuth, connectDB } from '../../../utils/features';
+import { Task } from '../../../models/task';
+import { asyncError, errorHandler } from '../../../middlewares/error';
+
+const handler = asyncError(async (req, res) => {
+  await connectDB();
+
+  const user = await checkAuth(req);
+  if (!user) return errorHandler(res, 401, 'Login First');
+  const taskId = req.query.id;
+  const task = await Task.findById(taskId);
+  if (!task) return errorHandler(res, 404, 'Task not found');
+  if (req.method === 'PUT') {
+    task.isCompleted = !task.isCompleted;
+
+    await task.save();
+
+    res.status(200).json({
+      success: true,
+      message: 'Task Updated successfully',
+    });
+  } else if (req.method === 'DELETE') {
+    await task.deleteOne();
+    res.status(200).json({
+      success: true,
+      message: 'Task Deleted successfully',
+    });
+  } else {
+    errorHandler(res, 400, 'This method is Not available');
+  }
+});
+
+export default handler;
